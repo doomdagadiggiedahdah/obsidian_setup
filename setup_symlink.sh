8e7a409e154c8b912586df0bbb7488593c9a6519
mat <matdallen@gmail.com>
2025-05-30 14:53:56 -0700

    fix recursion symlink

--- setup_symlink.sh
+++ setup_symlink.sh
@@ -4,8 +4,8 @@ DIR_LOCATION="$HOME/Documents/obsidian_setup" # replace this with this repo's lo
 OBS_LOCATION="$HOME/Obsidian"                 # replace this with your Obsidian root folder
 
 # Create symlinks
-ln -sf "${DIR_LOCATION}/snippets" "${OBS_LOCATION}/.obsidian/snippets"
-ln -sf "${DIR_LOCATION}/Templates" "${OBS_LOCATION}/Templates"
+ln -sfn "${DIR_LOCATION}/snippets" "${OBS_LOCATION}/.obsidian/snippets"
+ln -sfn "${DIR_LOCATION}/Templates" "${OBS_LOCATION}/Templates"
 ln -sf "${DIR_LOCATION}/week review checklist.md" "${OBS_LOCATION}/ZettleKasten/week review checklist.md"
 
 
